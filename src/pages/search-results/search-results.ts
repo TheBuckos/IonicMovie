132605bfe760a194a2886540721b3335a86dc059
David Arias <davidalexarias2@gmail.com>
2018-04-18 22:41:35 +0000

    fixed formatting of titles

--- src/pages/search-results/search-results.ts
+++ src/pages/search-results/search-results.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 import { SearchComponent } from '../../components/search/search'
